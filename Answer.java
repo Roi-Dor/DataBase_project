15216bca8d7f07cb43fb6231279e67cba377f26f
Roi Dor <Dor.Roi@s.afeka.ac.il>
2024-08-13 13:47:13 +0300

    test

--- Answer.java
+++ Answer.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
  * @author Daniel Grigoriev
  * @version 1.2
  */
-public class Answer implements Serializable {
+public class Answer implements Serializable {//infeaiubfuaek
     private String answer;
 
     public Answer(String answer) {
